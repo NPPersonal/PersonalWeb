12b153be4c11683df7b330ca5603ef8fb78b5c44
Nelson <tomneo2004@gmail.com>
2020-03-31 16:11:33 +0800

    remove not useded code

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import "assets/scss/material-kit-react.scss?v=1.8.0";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 import LoginPage from "views/LoginPage/LoginPage.js";
-import SectionSkills from 'views/Sections/SectionSkills.js';
 
 console.log(process.env.PUBLIC_URL);
 
@@ -19,7 +18,6 @@ ReactDOM.render(
       <Route exact path="/landing-page" component={LandingPage} />
       <Route exact path="/profile-page" component={ProfilePage} />
       <Route exact path="/login-page" component={LoginPage} />
-      <Route exact path="/skill" component={SectionSkills} />
       <Route path="/" component={ProfilePage} />
     </Switch>
   </BrowserRouter>,
